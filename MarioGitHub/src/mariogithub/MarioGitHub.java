527a36e7828668d32e6e71f15c5ab6e79f613c42
billiemillung <billiemillung@10.50.130.10>
2019-04-05 09:52:12 +0200

    KJfdhesfgknsæ

--- MarioGitHub/src/mariogithub/MarioGitHub.java
+++ MarioGitHub/src/mariogithub/MarioGitHub.java
@@ -9,16 +9,3 @@ public class MarioGitHub {
     }
 
 }
-==[IDE]== 05-04-2019 09:44:37 Validating destination...
-==[IDE]== 05-04-2019 09:44:37 Validating destination... finished.
-==[IDE]== 05-04-2019 09:44:37 Cloning
-git init /Users/billiemillung/NetBeansProjects/PizzariaMario
-git fetch https://github.com/BillieMIllung/Pizzaria.git +refs/heads/master:refs/remotes/origin/master
-Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
-Branch  : origin/master
-Old Id : null
-New Id : 13e7c17e2222913f266283fe67e800718164222c
-Result : NEW
-
-setting up remote: origin
-git branch --track master origin/master
